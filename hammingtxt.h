18b98e50fbbb1dcbc88b92897897e1e92df6bb2b
anieszt <nilsbohr1994@gmail.com>
2023-09-04 17:06:44 +0100

    continuation and modification of hamming function

--- hammingtxt.h
+++ hammingtxt.h
@@ -38,7 +38,7 @@ char *char_to_bin(char ch){
         return bin;    
     }
 
-    int bin_to_int(char *bin) {
+int bin_to_int(char *bin) {
     int n = 0;
     for (int i = 0; i < strlen(bin); i++) {
         n = n * 2 + (bin[i] - '0');
@@ -51,9 +51,32 @@ char bin_to_char(char *bin){
         return ch;
 }
 
+void *parity_bits(char *input) { //calculates parity bitsfor 1 char, outputs a string of those parity bits
+    char *parity = (char *)malloc(4 * sizeof(char));
+
+    //parity bits
+    parity[0] = (input[0] ^ input[1] ^ input[3]) + '0';
+    parity[1] = (input[0] ^ input[2] ^ input[3]) + '0';
+    parity[2] = (input[1] ^ input[2] ^ input[3]) + '0';
+
+    //even parity of the previous 7 bits
+    int even_parity = (input[0] - '0') + (input[1] - '0') + (input[2] - '0') + (input[3] - '0');
+    parity[4] = (even_parity % 2) + '0';
+
+    return parity;
+}
+
+char *hamm_enc_char(char *bin){ //inputs binary of 1 char and outputs hamming code of 1 char
+
+    
+    //int totL = strlen()
+    //char *code = (char *)malloc(totL * sizeof(char)); 
+     return NULL;
+}
+
 /*
-    int hamm(){ //returns output string and errors positions as accurately as possible
-        return 0;
+    int hamm_dec(){ //returns output string decoded
+    
     }
 
     void check_txt(char *txt_input /*hamm){ //loops output possibilities text
