e511a5c8ab05b3c48634667a87cb13abd0ab92b9
anieszt <nilsbohr1994@gmail.com>
2023-09-05 23:04:57 +0100

    error messages

--- Hammingtxt.c
+++ Hammingtxt.c
@@ -6,7 +6,7 @@ int main() {
     char a[500];
 
     if (fgets(a, sizeof(a), stdin) == NULL) {
-        perror("Error reading input");
+        perror("txt input mem error");
         return 1;
     }
 
@@ -19,14 +19,14 @@ int main() {
     char **b = (char **)malloc(len * sizeof(char *));
 
     if (b == NULL) {
-        perror("Memory allocation for b failed");
+        perror("txttobin individuals mem error");
         return 1;
     }
 
     for (size_t i = 0; i < len; i++) {
         b[i] = (char *)malloc((strlen(a) + 1) * sizeof(char));
         if (b[i] == NULL) {
-            perror("Memory allocation for b[i] failed");
+            perror("txttobin output mem error");
             for (size_t j = 0; j < i; j++) {
                 free(b[j]);
             }
@@ -38,7 +38,7 @@ int main() {
     char *parity = (char *)malloc(5 * sizeof(char));
 
     if (parity == NULL) {
-        perror("Memory allocation for parity failed");
+        perror("parity mem error");
         for (size_t i = 0; i < len; i++) {
             free(b[i]);
         }
@@ -61,45 +61,3 @@ int main() {
     free(parity);
     return 0;
 }
-
-
-
-
-
-
-
-
-/*int main(){
-    
-    char a[500];
-    fgets(a, sizeof(a), stdin);
-
-    char **b =  (char **) malloc(strlen(a) * sizeof(char *));
-
-    if (b == NULL) {
-        perror("Memory allocation for b failed");
-        return 1;
-    }
-
-    char *parity = (char *) malloc(5 * sizeof(char));
-
-        if (parity == NULL) {
-        perror("Memory allocation for parity failed");
-        free(b);
-        return 1; 
-    }
-
-    txt_to_bin(a, b);
-    for (size_t i = 0; i <= strlen(a); i++){
-        strcpy(parity, parity_bits(b[i]) );
-        printf("%c %s %s\n", bin_to_char(b[i]), b[i], parity);
-    }
-
-    for (size_t i = 0; i < strlen(a); i++) {
-        free(b[i]); 
-    }
-
-    free(b);
-    free(parity);
-    return 0;
-}*/
