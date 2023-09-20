0de2198e64870e2792bc61bb6e5624c9d42a6728
anieszt <nilsbohr1994@gmail.com>
2023-09-20 01:25:01 +0100

    detection and correction

--- hammingtxt.h
+++ hammingtxt.h
@@ -12,22 +12,27 @@ char *zero_fill(char *bin){
         strcat(bin, "0");       
     }
     bin[9]='\0';
+
     return strrev(bin);
 }
 
 char *int_to_bin(int L){
+
     char *bin = (char *) malloc(sizeof(char)*33);
     int i = 0;
     while(L > 0){
         bin[i++] = L%2 + '0';
         L /= 2;
     }
+
     bin[i] = '\0';
     strrev(bin);
+
     return bin;
 }
 
 void txt_to_bin(char *txt_input, char **bin_input){
+
         int txtL = strlen(txt_input);
         int *ascii_input = (int *) malloc(txtL * sizeof(int));
 
@@ -42,9 +47,11 @@ void txt_to_bin(char *txt_input, char **bin_input){
     }
 
 char *char_to_bin(char ch){
+
         char *bin = (char *) malloc(9 * sizeof(char));
         strcpy(bin,int_to_bin(toascii(ch)));
         bin[9]='\0';
+
         return bin;    
     }
 
@@ -57,8 +64,10 @@ int bin_to_int(char *bin) {
 }
 
 char bin_to_char(char *bin){
+
         char ch = (char) bin_to_int(bin);
         return ch;
+
 }
 
 void *parity_bits(char *bin) { //calculates parity bitsfor 1 char, outputs a string of those parity bits
@@ -90,7 +99,7 @@ char *hamm_enc(char *bin){ //inputs binary of 1 char and outputs hamming code of
 }
 
 
-char *hamm_dec(char *bit_hamm){ //returns output string decoded
+char hamm_dec(char *bit_hamm){ //returns output string decoded
 
     if(strlen(bit_hamm) != 12){
     printf("Error : input must be 12 bits");
@@ -103,31 +112,28 @@ char *hamm_dec(char *bit_hamm){ //returns output string decoded
     for(size_t i = 0; i < 4; i++){
         input_parity[i] = bit_hamm[i];
     }
+
     for(size_t i = 4; i < 12; i++){
         bit[i-4] = bit_hamm[i];
-        
     }
 
     char calculated_parity[4];
     strcpy(calculated_parity,parity_bits(bit));
 
-    char error = '0';
+    char e = '0';
     for (int i = 0; i < 4; i++) {
-        if (parity[i] != calculated_parity[i]) {
-            error |= (1 << i);
+        if (input_parity[i] != calculated_parity[i]) {
+            e |= (1 << i);
         }
     }
-    return calculated_parity;
+
+    if (e != '0'){
+        int e_index = e - '0' - 1;
+        bit[e_index] ^= '1';
+
+        printf("error index: %d", e_index);
+    }
+
+    char ch = bin_to_char(bit);
+    return ch;
 }
-/*
-    void check_txt(char *txt_input *hamm){ //loops output possibilities text
-    }
-
-    void check_bin(char *bin /*hamm){ //loops output possibilities binary
-    }
-
-    int compare(char *str){
-        
-        return p; //p: probabilty
-    }
-*/
